7469eca9aa90df473022622fc34d053a98385dec
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-01-23 18:29:25 -0800

    Update 04.cpp

--- 04.cpp
+++ 04.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 // playing with operator overloading, constructors destructors heap object
 // allocation everything of the sort
-
 class value{
 public:
     value(int x) {
@@ -29,17 +28,13 @@ public:
         sleep(LEN);
     }
 };
-
 int main(){
     // allocating new dynamic objects on the heap
     value* ob1 = new value(3.1415);
-
     // same as previous example just longer
     value* ob2;
     ob2 = new value("Jerry");
-
     // this object will be init'd and stored on the stack
     value ob3(12);
-
     return 0;
 }
