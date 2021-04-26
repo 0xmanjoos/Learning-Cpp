f7f35fe8f22bbfa1b54d1d75a58f1f10b17fd3e9
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-04-26 01:02:53 -0700

    Update cowsay.c

--- cowsay.c
+++ cowsay.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#define MAX 50
 // gonna write this in x86 assembler :/
 int main(int argc,char**argv) {
-    if (argc!=2)goto die;
+    char buffer[MAX];
+    if (argc<2)goto die;
     // ---- \n < hello > \n ------ \n
-    int len=strlen(argv[1]),border=0;
+    for (int i=1;i<argc;i++) {
+        size_t d_len = strlen(buffer), z;//dest
+        for (z=0;argv[i][z]!='\0';z++) {
+            buffer[d_len+z]=argv[i][z];
+        }
+        if(i!=argc-1)buffer[d_len+z]=' ';
+        buffer[d_len+z+1]='\0';
+    }
+    int len=strlen(buffer),border=0;
     write(1," ",1);
     do {
         write(1,"_",1);
         border++;
     } while (border<=len+1);
     border=0;
-    printf("\n< %s >\n",argv[1]);
+    printf("\n< %s >\n",buffer);
     write(1," ",1);
     do {
         write(1,"-",1);
@@ -24,5 +35,6 @@ int main(int argc,char**argv) {
     "            (__)\\       )\\/\\ \n"
     "                ||----w | \n"
     "                ||     || ");
-die: return 0;
+    exit(0);
+die: puts("Error!"); return 0;
 }
